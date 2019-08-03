7ecdb4c6236934dde89dcb5073ecf0d6d5fe0b9b
cfox <cfox.ma@gmail.com>
2019-08-03 22:38:13 +0800

    MediaSimple:delete lib
    Signed-off-by: cfox <cfox.ma@gmail.com>

--- lib_audio/src/main/java/com/cfox/rpaudio/recorder/wav/IAudioStatusListener.java
+++
@@ -1,5 +0,0 @@
-package com.cfox.rpaudio.recorder.wav;
-
-public interface IAudioStatusListener {
-    void statusChange(int status);
-}
--- lib_audio/src/main/java/com/cfox/rpaudio/recorder/wav/IAudioWaveDataListener.java
+++
@@ -1,5 +0,0 @@
-package com.cfox.rpaudio.recorder.wav;
-
-public interface IAudioWaveDataListener {
-    void waveData(byte[] audioData);
-}
